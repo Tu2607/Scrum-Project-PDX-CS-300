1a1661c1372d1cff15c64e0ecabd8dd452794f1e
Jake Klusnick <jklusnick@gmail.com>
2019-11-08 23:21:53 -0800

    energy and supplies checking functions

--- SpaceHunt/classes.js
+++ SpaceHunt/classes.js
@@ -1,6 +1,18 @@
 class Ship {
-  constructor(energy) {
+  constructor(energy, supplies) {
     this.energy = energy;
+    this.supplies = supplies;
   }
   
+  checkSupplies() {
+    if (this.supplies < 1) {
+      alert("You've run out of supplies! Game over.");
+    }
+  }
   
+  checkEnergy() {
+    if (this.energy < 1) {
+      alert("You've run out of energy! Game over.");
+    }
+  }
+}
