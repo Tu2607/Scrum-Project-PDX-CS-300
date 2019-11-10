f012c2a892f50b09388e1fcd495325bf03c22f23
Tu Vu <tuvu2607@gmail.com>
2019-11-09 21:14:15 -0800

    Working checkboxes

--- SpaceHunt/Config.js
+++ SpaceHunt/Config.js
@@ -1,7 +1,18 @@
-function updateConfig (energy,resource,cheat,normal){
+function updateConfig (energy,resource,cheat,normal,random,fix){
 	energy = eval(energy) + 0;
 	resource = eval(resource) + 0;
 
+	if(cheat){
+		alert("You have actived cheat mode!");
+		// call function to change setting of the game to undying
+	}
+
+	if(normal){
+		alert("You have decided to play normally.");
+	}
+
 	Ship.energy.value = energy;
 	Ship.resource.value = resource;	
+
+
 }
