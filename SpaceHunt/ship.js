2a4f8199fcc304e7509eca66bb8c65287e765522
Tu Vu <tuvu2607@gmail.com>
2019-11-14 12:48:19 -0800

    Adding function that "could" update gamesetting

--- SpaceHunt/ship.js
+++ SpaceHunt/ship.js
@@ -19,4 +19,33 @@ class Ship {
       gameOver = True;
     }
   }
+  
+  activeCheat(){
+	if(Ship.energy.value < 0){
+		//change setting so that you don't die
+	}
+  }
+
+//function updateConfig (energy,resource,cheat,normal,random,fix,xcord,ycord){
+  updateConfig(config){
+	var energy = eval(config.energy.value) + 0;
+	var resource = eval(config.energy.value) + 0;
+	var xcord = eval(config.xcord.value) + 0;
+	var ycord = eval(config.ycord.value) + 0;
+
+	if(config.normal){
+		//does nothing I guess
+	}
+
+	if(config.GodMode){
+		// have a flag that mark the game that turned on the cheat
+		activeCheat();
+	}
+
+	this.energy. = energy;
+	this.resource = resource;	
+	this.x = xcord;
+	this.y = ycord; 
+	
+  }
 }
