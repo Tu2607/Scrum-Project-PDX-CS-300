41a24d617ac5316fa78f313b36362fadd3d42808
Tu Vu <tuvu2607@gmail.com>
2019-11-19 00:18:27 -0800

    Update ship.js

--- SpaceHunt/ship.js
+++ SpaceHunt/ship.js
@@ -47,8 +47,8 @@ function move(UI, canvas, angle, distance, config) // Tu's note: Pass in config 
   if(ship.yPos > space.ySize || ship.yPos <= 0 || ship.xPos > space.xSize || ship.xPos <= 0){
     // Random Worm Hole Case
     if(config.wormhole.value == "random"){
-      ship.yPos = getRandom(0,(space.xSize+1));    
-      ship.xPos = getRandom(0,(space.xSize+1));
+      ship.yPos = getRandom(0,15) * 8;    
+      ship.xPos = getRandom(0,15) * 8;
     } else if (config.wormhole.value == "fixed"){   //Fixed worm hole case
       ship.yPos = 50;
       ship.xPos = 50;
