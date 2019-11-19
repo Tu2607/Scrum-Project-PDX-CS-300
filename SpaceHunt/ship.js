5fe84111525ed964e598fe1393d3a105ed95514a
Kelly Burton <kebu2@pdx.edu>
2019-11-19 01:34:03 -0800

    moved function that checked ships position vs boundry down to after the movement

--- SpaceHunt/ship.js
+++ SpaceHunt/ship.js
@@ -43,17 +43,6 @@ function move(UI, canvas, angle, distance, config) // Tu's note: Pass in config 
   var visitedPoints = JSON.parse(sessionStorage.getItem("visitedPoints"));
   var ship = JSON.parse(sessionStorage.getItem("ship"));
 
-  //This is the case where the user move out of bounds and activated the wormhole behavior
-  if(ship.yPos > space.ySize || ship.yPos <= 0 || ship.xPos > space.xSize || ship.xPos <= 0){
-    // Random Worm Hole Case
-    if(config.wormhole.value == "random"){
-      ship.yPos = getRandom(0,15) * 8;    
-      ship.xPos = getRandom(0,15) * 8;
-    } else if (config.wormhole.value == "fixed"){   //Fixed worm hole case
-      ship.yPos = 50;
-      ship.xPos = 50;
-    }
-  }
 
   // Up
   if(angle == 90)
@@ -80,6 +69,17 @@ function move(UI, canvas, angle, distance, config) // Tu's note: Pass in config 
     // Check Energy and Supplies
     checkEnergy(ship) 
     checkSupplies(ship)
+  }
+  //This is the case where the user move out of bounds and activated the wormhole behavior
+  if(ship.yPos > space.ySize || ship.yPos <= 0 || ship.xPos > space.xSize || ship.xPos <= 0){
+    // Random Worm Hole Case
+    if(config.wormhole.value == "random"){
+      ship.yPos = getRandom(0,15) * 8;    
+      ship.xPos = getRandom(0,15) * 8;
+    } else if (config.wormhole.value == "fixed"){   //Fixed worm hole case
+      ship.yPos = 50;
+      ship.xPos = 50;
+    }
   }
 
 
