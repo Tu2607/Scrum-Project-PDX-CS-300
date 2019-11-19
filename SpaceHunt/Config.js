75c774328e2dd3a69ee3b9a8d70dbeae554dccc4
kburton501 <kebu2@pdx.edu>
2019-11-19 11:02:15 -0800

    commented out canvas.* =eval(*)

--- SpaceHunt/Config.js
+++ SpaceHunt/Config.js
@@ -7,8 +7,8 @@ function updateConfig(config, UI, canvas, space, ship, cheat, artifactSet, visit
 	artifactSet = new Array();
 	visitedPoints = new Array();
 	/* For the max size*/
-	canvas.width = eval(config.xMax.value);
-	canvas.height = eval(config.yMax.value);
+	//canvas.width = eval(config.xMax.value);
+	//canvas.height = eval(config.yMax.value);
 
 	space = new Space(canvas.width, canvas.height);
 	ship = new Ship(8 * eval(config.xout.value), 8 * eval(config.yout.value), eval(config.energy.value), eval(config.supplies.value),eval(config.credits.value));
