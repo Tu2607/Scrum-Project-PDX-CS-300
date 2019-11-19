d9372600e3412ee990428d45b587c4b463ea3306
Bruh Beyene <bbeyene@gmail.com>
2019-11-18 21:30:41 -0800

    Update ship.js

--- SpaceHunt/ship.js
+++ SpaceHunt/ship.js
@@ -76,9 +76,11 @@ function move(UI, canvas, angle, distance, config) // Tu's note: Pass in config 
   useSupplies(ship, 2)
   useEnergy(ship, distance)
 
+if(config.cheatMode == false) {
   // Check Energy and Supplies
   checkEnergy(ship) 
   checkSupplies(ship)
+}
 
 
   addVisitedPoint(visitedPoints, ship.xPos, ship.yPos)
