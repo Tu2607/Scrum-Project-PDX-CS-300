33e16251e218ff91909bb27368dc0f674cfa3344
Bruh Beyene <bbeyene@gmail.com>
2019-11-23 06:48:05 -0800

    removed commented-out duplicate

--- SpaceHunt/Config.js
+++ SpaceHunt/Config.js
@@ -1,6 +1,4 @@
 
-// TU's ORIGINAL CODE BELOW
-
 function updateConfig(space, ship, cheat, artifactSet, visitedPoints) {
 
 	artifactSet = new Array();
@@ -50,44 +48,3 @@ function updateStatus(xPos, yPos, energy, supplies, credits) {
 function sensorStatus(supplies){
 	UI.supplies.value = supplies;
 }
-
-
-/*
-function blackhole(UI)
-
-
-
-function activeCheat(){
-	if(Ship.energy.value < 0){
-		//change setting so that you don't die
-	}
-
-
-}
-
-
-
-//function updateConfig (energy,resource,cheat,normal,random,fix,xcord,ycord){
-
-function updateConfig(config){
-	var energy = eval(config.energy.value) + 0;
-	var resource = eval(config.energy.value) + 0;
-	var xcord = eval(config.xcord.value) + 0;
-	var ycord = eval(config.ycord.value) + 0;
-
-	if(config.normal){
-		//does nothing I guess
-	}
-
-	if(config.GodMode){
-		// have a flag that mark the game that turned on the cheat
-		activeCheat();
-	}
-
-	Ship.energy.value = energy;
-	Ship.resource.value = resource;	
-	Ship.x.value = xcord;
-
-	Ship.y.value = ycord;
-
-}*/
