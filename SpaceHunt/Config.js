a762cc0fda81980a245eeb3afcac1791dcd4e354
Tu Vu <tuvu2607@gmail.com>
2019-11-25 21:37:28 -0800

    Can resize a working map. Button on click is buggy

--- SpaceHunt/Config.js
+++ SpaceHunt/Config.js
@@ -9,10 +9,11 @@ function updateConfig(space, ship, cheat, artifactSet, visitedPoints) {
 
 	//create the environment from inputs
 	//the size of the game
-	space = new Space(8 * eval(config.xMaxOut.value), 8 * eval(config.yMaxOut.value));
+	//space = new Space(8 * eval(config.xMaxOut.value), 8 * eval(config.yMaxOut.value));
+	space = new Space(8*eval(config.mapSize.value), 8*eval(config.mapSize.value));
 
 	//the ship's starting coordinates and status 
-	ship = new Ship(8 * eval(config.xout.value), 8 * (eval(config.xMaxOut.value) - eval(config.yout.value)), eval(config.energy.value), eval(config.supplies.value), eval(config.credits.value), false);
+	ship = new Ship(8 * eval(config.xout.value), 8 * (eval(config.mapSize.value) - eval(config.yout.value)), eval(config.energy.value), eval(config.supplies.value), eval(config.credits.value), false);
 
 	//set the visibility of artifacts (true for all if cheat mode checked)
 	buildArtifactSet(config.cheatMode.checked, artifactSet);
@@ -37,7 +38,7 @@ function updateConfig(space, ship, cheat, artifactSet, visitedPoints) {
 //updates status fields (on index page) with game status
 function updateStatus(xPos, yPos, energy, supplies, credits) {
 	UI.xValue.value = xPos/8
-	UI.yValue.value = (eval(config.yMaxOut.value)*8 - yPos)/8;
+	UI.yValue.value = (eval(config.mapSize.value)*8 - yPos)/8;
 	UI.energy.value = energy;
 	UI.supplies.value = supplies;
 	UI.credits.value = credits;
