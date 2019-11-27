5e6e211d1263a07a68982368024d726147231e80
Kelly Burton <kebu2@pdx.edu>
2019-11-27 12:34:06 -0800

    added 8 multiply  for better planet placement

--- SpaceHunt/artifact.js
+++ SpaceHunt/artifact.js
@@ -49,20 +49,20 @@ function buildArtifacts(artifactSet)
 {
     while(artifactSet.pop());   // THIS BECAUSE WE WANT THE OLD ONE OUTTA HERE!!
 
-    artifactSet.push(new Artifact(admin.xeniac.value, admin.yeniac.value, "eniac", "orange", true));
-    artifactSet.push(new Artifact(admin.xmoon.value, admin.ymoon.value, "moon", "white", true));
-    artifactSet.push(new Artifact(admin.xceleron.value, admin.yceleron.value, "celeron", "yellow", true));
-    artifactSet.push(new Artifact(admin.xryzen.value, admin.yryzen.value, "ryzen", "red", true));
-    artifactSet.push(new Artifact(admin.xxeon.value, admin.yxeon.value, "xeon", "blue", true));
-    artifactSet.push(new Artifact(admin.xpentium1.value, admin.ypentium1.value, "pentium 1", "purple", false));
-    artifactSet.push(new Artifact(admin.xpentium2.value, admin.ypentium2.value, "pentium 2", "purple", false));
-    artifactSet.push(new Artifact(admin.xpentium3.value, admin.ypentium3.value, "pentium 3", "purple", false));
-    artifactSet.push(new Artifact(admin.xpentium4.value, admin.ypentium4.value, "pentium 4", "purple", false));
-    artifactSet.push(new Artifact(admin.xpentium5.value, admin.ypentium5.value, "pentium 5", "purple", false));
-    artifactSet.push(new Artifact(admin.xpentium6.value, admin.ypentium6.value, "pentium 6", "purple", false));
-    artifactSet.push(new Artifact(admin.xpentium7.value, admin.ypentium7.value, "pentium 7", "purple", false));
-    artifactSet.push(new Artifact(admin.xasteroid1.value, admin.yasteroid1.value, "asteroid 1", "grey", false));
-    artifactSet.push(new Artifact(admin.xasteroid2.value, admin.yasteroid2.value, "asteroid 2", "grey", false));
-    artifactSet.push(new Artifact(admin.xasteroid3.value, admin.yasteroid3.value, "asteroid 3", "grey", false));
-    artifactSet.push(new Artifact(admin.xasteroid4.value, admin.yasteroid4.value, "asteroid 4", "grey", false));
+    artifactSet.push(new Artifact((admin.xeniac.value*8), ( admin.yeniac.value*8), "eniac", "orange", true));
+    artifactSet.push(new Artifact((admin.xmoon.value*8), ((admin.ymoon.value*8)), "moon", "white", true));
+    artifactSet.push(new Artifact((admin.xceleron.value*8), (admin.yceleron.value*8), "celeron", "yellow", true));
+    artifactSet.push(new Artifact((admin.xryzen.value*8), (admin.yryzen.value*8), "ryzen", "red", true));
+    artifactSet.push(new Artifact((admin.xxeon.value*8), (admin.yxeon.value*8), "xeon", "blue", true));
+    artifactSet.push(new Artifact((admin.xpentium1.value*8), (admin.ypentium1.value*8), "pentium 1", "purple", false));
+    artifactSet.push(new Artifact((admin.xpentium2.value*8), (admin.ypentium2.value*8), "pentium 2", "purple", false));
+    artifactSet.push(new Artifact((admin.xpentium3.value*8), (admin.ypentium3.value*8), "pentium 3", "purple", false));
+    artifactSet.push(new Artifact((admin.xpentium4.value*8), (admin.ypentium4.value*8), "pentium 4", "purple", false));
+    artifactSet.push(new Artifact((admin.xpentium5.value*8), (admin.ypentium5.value*8), "pentium 5", "purple", false));
+    artifactSet.push(new Artifact((admin.xpentium6.value*8), (admin.ypentium6.value*8), "pentium 6", "purple", false));
+    artifactSet.push(new Artifact((admin.xpentium7.value*8), (admin.ypentium7.value*8), "pentium 7", "purple", false));
+    artifactSet.push(new Artifact((admin.xasteroid1.value*8), (admin.yasteroid1.value*8), "asteroid 1", "grey", false));
+    artifactSet.push(new Artifact((admin.xasteroid2.value*8), (admin.yasteroid2.value*8), "asteroid 2", "grey", false));
+    artifactSet.push(new Artifact((admin.xasteroid3.value*8), (admin.yasteroid3.value*8), "asteroid 3", "grey", false));
+    artifactSet.push(new Artifact((admin.xasteroid4.value*8), (admin.yasteroid4.value*8), "asteroid 4", "grey", false));
 }
